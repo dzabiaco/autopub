8927b8ed7ffa082f58e8a8a2c87132be2c990109
Dmitri <dzabiaco@outlook.com>
2021-02-19 14:41:11 +0200

    first commit

---
+++ src/test/java/com/autopub/Initialization.java
@@ -0,0 +1,43 @@
+package com.autopub;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Initialization {
+	
+	private static WebDriver driver;
+	private static String url ="https://999.md/ru/";
+	private static String username = "sennetta";
+	private static String email ="ssennetta@gmail.com";
+	private static String password = "JavaScript8*";
+
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		String path = System.getProperty("user.dir") + "\\chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", path);
+		
+		driver =  new ChromeDriver();
+		driver.get(url);
+		
+		WebElement enter_link = driver.findElement(By.xpath("//a[@id='']"));
+		enter_link.click();
+		
+		WebElement login_input = driver.findElement(By.xpath("//body/div[1]/div[1]/form[1]/div[2]/input[1]"));
+		login_input.sendKeys(username);
+		
+		WebElement password_input = driver.findElement(By.xpath("//body/div[1]/div[1]/form[1]/div[3]/input[1]"));
+		password_input.sendKeys(password);
+		
+		WebElement login_button = driver.findElement(By.xpath("//button[contains(text(),'Войти')]"));
+		login_button.click();
+		
+	}
+
+}
