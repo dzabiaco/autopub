5826d2a2999261ca4ca6ea6fbcb23f14258f9f3e
Dmitri <dzabiaco@outlook.com>
2021-02-20 05:10:21 +0300

    actions steps

--- src/test/java/com/autopub/Initialization.java
+++ src/test/java/com/autopub/Initialization.java
@@ -9,22 +9,26 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Initialization {
 	
 	private static WebDriver driver;
-	private static String url ="";
-	private static String username = "";
-	private static String email ="";
-	private static String password = "";
+	private static String url ="https://999.md/ru/";
+	private static String username = "ssennetta";
+	private static String email ="sennetta@gmail.com";
+	private static String password = "JavaScript8*";
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws FileNotFoundException, InterruptedException {
 		// TODO Auto-generated method stub
 		String path = System.getProperty("user.dir") + "\\chromedriver.exe";
 		System.setProperty("webdriver.chrome.driver", path);
 		
 		driver =  new ChromeDriver();
 		driver.get(url);
+		driver.manage().deleteAllCookies();
 		
 		WebElement enter_link = driver.findElement(By.xpath("//a[@id='']"));
 		enter_link.click();
@@ -37,6 +41,17 @@ public class Initialization {
 		
 		WebElement login_button = driver.findElement(By.xpath("//button[contains(text(),'Войти')]"));
 		login_button.click();
+		
+		Thread.sleep(2000);
+		
+		WebElement cabinet = driver.findElement(By.xpath("//header/div[1]/nav[1]/div[1]"));
+		WebElement obiava = driver.findElement(By.xpath("//header/div[1]/nav[1]/div[1]/div[1]/ul[1]/li[1]/a[1]"));
+		WebElement ob = driver.findElement(By.cssSelector("body.page.page-index:nth-child(3) div.wrapper:nth-child(6) div.header_menu nav.header_menu_nav div.header_menu_nav__dropdown-wrapper div:nth-child(1) ul:nth-child(1) li:nth-child(1) > a:nth-child(1)"));
+		
+		WebDriverWait wait = new WebDriverWait(driver, 5);
+		Actions actions = new Actions(driver);
+		actions.moveToElement(cabinet).perform();
+		wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Объявления"))).click();
 		
 	}
 
